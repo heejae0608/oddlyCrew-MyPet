e294efe22e81d507f48039304f1f16d3b46f3a1e
희재 <heejae0608@gmail.com>
2025-09-17 22:11:10 +0900

    Initial Commit

---
+++ mypet/mypetApp.swift
@@ -0,0 +1,17 @@
+//
+//  mypetApp.swift
+//  mypet
+//
+//  Created by 전희재 on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct mypetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
